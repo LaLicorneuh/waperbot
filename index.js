3d7d08004e876d14db1049e7246dccbfaa72c462
LaLicorneuh <46375238+LaLicorneuh@users.noreply.github.com>
2019-01-04 19:40:22 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var Discord = require('discord.js');
-var bot = new Discord.Client();
-
-bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: '[!help] | WaPerBot', type: 0 } })
-    console.log('Bot Launched...')
-});
-
-bot.login(process.env.TOKEN)
